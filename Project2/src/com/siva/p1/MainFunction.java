a1ffb96c718844169a1c62dd9d8b147d0b263158
siva k <siva.k@in.abb.com>
2019-04-16 14:28:59 +0530

    I have modified project2 main class added sysout statement

--- Project2/src/com/siva/p1/MainFunction.java
+++ Project2/src/com/siva/p1/MainFunction.java
@@ -5,6 +5,7 @@ public class MainFunction {
 	public static void main(String[] args) {
 		
 		System.out.println("First Comit in Project 2  ");
+		System.out.println("Second  Comit in Project 2 by Siva K  ");
 	}
 
 }
